786e1c946a5808c76c25ca021ab036e8f23573f1
LANS <nazarsalomon123@gmail.com>
2025-04-10 20:30:01 -0600

    Cors

--- src/main/java/com/salomon/prueba/curd/config/cors.java
+++ src/main/java/com/salomon/prueba/curd/config/cors.java
@@ -13,7 +13,7 @@ public class cors {
             @SuppressWarnings("null")
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Aplica a todos los endpoints
+                registry.addMapping("/**")
                         .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500")
                         .allowedMethods("*")
                         .allowedHeaders("*");
